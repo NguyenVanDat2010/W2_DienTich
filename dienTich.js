f98e31e2cafd7f2e2efacebf926c84b618772db2
Kira <datdhvinh@gmail.com>
2020-03-30 16:47:40 +0700

    add dienTich.html file

---
+++ dienTich.js
@@ -0,0 +1,6 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("Nhập chiều rộng: ");
+inputheight = prompt("Nhập chiều dài: ");
+let S = inputwidth*inputheight;
+document.write("Diện tích là: "+S);
